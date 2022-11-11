05253445ae9a0aec1d9665c8d478271f836211a2
Kyle Kaminky <kyle.kaminky@gmail.com>
2022-11-10 20:33:31 -0700

    More work

--- beginning-elm/src/Playground.elm
+++ beginning-elm/src/Playground.elm
@@ -1,14 +1,21 @@
 module Playground exposing
-    ( Greeting(..)
+    ( Character
+    , Greeting(..)
+    , MyList(..)
     , add
+    , arya
     , descending
     , doubleScores
     , escapeEarth
+    , getAdultAge
     , guardiansWithShortNames
     , highestScores
     , main
+    , sansa
     , sayHello
     , scoresLessThan320
+    , signUp
+    , sum
     , validateEmail
     )
 
@@ -190,6 +197,7 @@ validateEmail email =
         ( "Invalid email", "red" )
 
 
+outerMultiplier : number
 outerMultiplier =
     6
 
@@ -203,6 +211,7 @@ multiplyByFive number =
     number * multiplier
 
 
+scoreMultiplier : number
 scoreMultiplier =
     2
 
@@ -247,6 +256,8 @@ add num1 num2 =
 type Greeting
     = Howdy
     | Hola
+    | Namaste String
+    | NumericalHi Int Int
 
 
 sayHello : Greeting -> String
@@ -257,6 +268,89 @@ sayHello greeting =
 
         Hola ->
             "Hola amigo!"
+
+        Namaste message ->
+            message
+
+        NumericalHi value1 value2 ->
+            value1 + value2 |> String.fromInt
+
+
+signUp : String -> String -> Result String String
+signUp email ageStr =
+    case String.toInt ageStr of
+        Nothing ->
+            Err "Age must be an integer."
+
+        Just age ->
+            let
+                emailPattern =
+                    "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+                regex =
+                    Maybe.withDefault Regex.never <|
+                        Regex.fromString emailPattern
+
+                isValidEmail =
+                    Regex.contains regex email
+            in
+            if age < 13 then
+                Err "You need to be at least 13 years old to sign up."
+
+            else if isValidEmail then
+                Ok "Your account has been created successfully!"
+
+            else
+                Err "You entered an invalid email."
+
+
+type alias Character =
+    { name : String
+    , age : Maybe Int
+    }
+
+
+sansa : Character
+sansa =
+    { name = "Sansa"
+    , age = Just 19
+    }
+
+
+arya : Character
+arya =
+    { name = "Arya"
+    , age = Nothing
+    }
+
+
+getAdultAge : Character -> Maybe Int
+getAdultAge character =
+    case character.age of
+        Nothing ->
+            Nothing
+
+        Just age ->
+            if age >= 18 then
+                Just age
+
+            else
+                Nothing
+
+
+type MyList a
+    = Empty
+    | Node a (MyList a)
+
+
+sum : MyList Int -> Int
+sum myList =
+    case myList of
+        Empty ->
+            0
+
+        Node intValue remainingNodes ->
+            intValue + sum remainingNodes
 
 
 main : Html.Html msg
